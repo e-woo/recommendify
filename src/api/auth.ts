773433f1c80ed99e9cce49b03a00cab966abf17b
ethanw <ethansfwoo@gmail.com>
2023-12-06 22:45:37 -0700

    update redirect uri

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,7 +6,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append('client_id', clientId);
     params.append('response_type', 'code');
-    params.append('redirect_uri', 'http://localhost:3000');
+    params.append('redirect_uri', 'https://recommendify.netlify.app');
     params.append('scope', 'user-read-private user-read-email playlist-modify-public playlist-modify-private');
     params.append('code_challenge_method', 'S256');
     params.append('code_challenge', challenge);
@@ -40,7 +40,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append('client_id', clientId);
     params.append('grant_type', 'authorization_code');
     params.append('code', code);
-    params.append('redirect_uri', 'http://localhost:3000');
+    params.append('redirect_uri', 'https://recommendify.netlify.app');
     params.append('code_verifier', verifier!);
 
     const result = await fetch('https://accounts.spotify.com/api/token', {
