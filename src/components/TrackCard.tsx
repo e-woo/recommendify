791b811e32de1728daa82003eb012fd1bf49b604
ethanw <ethansfwoo@gmail.com>
2024-02-12 01:02:22 -0700

    Added limit to size of title for track cards

--- src/components/TrackCard.tsx
+++ src/components/TrackCard.tsx
@@ -5,7 +5,7 @@ const ArtistCard = ({ track, onClick } : {track: Track, onClick: Function}) => {
 		<div className='grid grid-cols-5 gap-2 px-2' onClick={() => onClick(track)}>
 			<img src={track.image} className='col-span-1 place-self-center'/>
 			<div className='col-span-3'>
-				<h5 className='text-white'>{track.name}</h5>
+				<h5 className='text-white'>{track.name.length > 40 ? track.name.substring(0, 40) + "..." : track.name}</h5>
 				<p className='text-slate-400 text-sm'>{
 					// add commas between artists if there are multiple
 					track.artists.map((artist: any, index: number) => artist + (index == track.artists.length - 1 ? '' : ', '))
